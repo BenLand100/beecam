43eaadb4bbe80d519daa89c54d58075220874f28
Benjamin Land <benland100@gmail.com>
2024-09-16 20:51:32 -0400

    Fix paths

--- hive2-beecounter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /var/lib/beecam
-
-while true; do
-    /opt/tf_venv/wrap ./beecounter2.py -e hive2_counts.sqlite /var/lib/beecam/stream/hive2.m3u8;
-    echo 'Python process terminated'
-    sleep 10;
-done
