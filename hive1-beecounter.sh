4338b9f435d009151a7d1f547aa2c794ad7af2a6
Benjamin Land <benland100@gmail.com>
2024-09-16 20:29:49 -0400

    Initial scripts

---
+++ hive1-beecounter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /var/lib/beecam
+
+while true; do
+    /opt/tf_venv/wrap ./beecounter2.py -e hive1_counts.sqlite /var/lib/beecam/stream/hive1.m3u8;
+    echo 'Python process terminated'
+    sleep 10;
+done
