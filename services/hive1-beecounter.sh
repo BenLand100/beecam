fe47544e1d0cb35022dd27cd61c723b8638217d1
Benjamin Land <benland100@gmail.com>
2024-09-25 23:45:28 -0400

    Pass PID onto actual service.

--- services/hive1-beecounter.sh
+++ services/hive1-beecounter.sh
@@ -2,8 +2,4 @@
 
 cd /var/lib/beecam
 
-while true; do
-    /opt/tf_venv/wrap ./beecounter2.py -e hive1_counts.sqlite -m model_v5.h5 /var/lib/beecam/stream/hive1.m3u8;
-    echo 'Python process terminated'
-    sleep 10;
-done
+exec /opt/tf_venv/wrap ./beecounter2.py hive1_counts.sqlite -m model_v5.h5 /var/lib/beecam/webroot/stream/hive1.m3u8;
--- services/hive2-beecounter.sh
+++ services/hive2-beecounter.sh
@@ -2,8 +2,4 @@
 
 cd /var/lib/beecam
 
-while true; do
-    /opt/tf_venv/wrap ./beecounter2.py -e hive2_counts.sqlite -m model_v5.h5 /var/lib/beecam/stream/hive2.m3u8;
-    echo 'Python process terminated'
-    sleep 10;
-done
+exec /opt/tf_venv/wrap ./beecounter2.py hive2_counts.sqlite -m model_v5.h5 /var/lib/beecam/webroot/stream/hive2.m3u8;
