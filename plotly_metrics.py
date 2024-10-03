81fcdd9a5c254088e65efe77cc4bf9b06ae714f5
Benjamin Land <benland100@gmail.com>
2024-10-02 21:33:18 -0400

    Install sensors on hive1

--- plotly_metrics.py
+++ plotly_metrics.py
@@ -12,7 +12,8 @@ import os
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-def load_data(count_con, metric_con, min_date='2024-04-15', interval=5*60):    
+
+def load_data(count_con, metrics_con, min_date, interval):    
     df_recent = pd.read_sql_query(f"""
         SELECT 
             datetime(floor(unixepoch(timestamp)/{interval})*{interval}, 'unixepoch') as timebin,
@@ -27,13 +28,13 @@ def load_data(count_con, metric_con, min_date='2024-04-15', interval=5*60):
         SELECT 
             datetime(floor(unixepoch(coalesce(f.timestamp,s.timestamp))/{interval})*{interval}, 'unixepoch') as timebin,
             10*log10(avg(ambient_lux)/1000) as 'Brightness (dB-klux)', 
-            --avg(ambient_lux)/1000 as 'Illumination (klux)', 
+            --avg(ambient_lux) as 'Illumination (lux)', 
             2.205*avg(weight) as 'Weight (lbs)', 
             avg(ext_temperature) as 'Temp-Barometer (C)', 
             (avg(ext_pressure)-95)*10 as 'ΔPressure (mbar)',
             avg(temperature_0)*9/5+32 as 'Temp-Under (F)', avg(humidity_0) as 'Humidity-Under (%)',
             avg(temperature_1)*9/5+32 as 'Temp-Brood (F)', avg(humidity_1) as 'Humidity-Brood (%)',
-            avg(temperature_2)*9/5+32 as 'Temp-2 (F)', avg(humidity_2) as 'Humidity-2 (%)',
+            avg(temperature_2)*9/5+32 as 'Temp-Top (F)', avg(humidity_2) as 'Humidity-Top (%)',
             avg(temperature_3)*9/5+32 as 'Temp-3 (F)', avg(humidity_3) as 'Humidity-3 (%)',
             avg(temperature_4)*9/5+32 as 'Temp-4 (F)', avg(humidity_4) as 'Humidity-4 (%)'
         FROM fast_sensors f
@@ -42,7 +43,7 @@ def load_data(count_con, metric_con, min_date='2024-04-15', interval=5*60):
         WHERE f.timestamp >= '{min_date}' or s.timestamp >= '{min_date}'
         GROUP BY timebin
         ORDER BY timebin
-    """, metric_con)
+    """, metrics_con)
 
     return df_metrics.merge(df_recent, on='timebin', how='outer')
 
@@ -56,22 +57,24 @@ def plot_data(df_combined):
         if ~df_combined[metric].isnull().all():
             fig.add_trace(
                 go.Scatter(x=df_combined.timebin, y=df_combined[metric], name=metric),
-                secondary_y=('lux' in metric) # or 'mbar' in metric) #or 'Bee' in metric),
+                secondary_y=('lux' in metric or '(C)' in metric or 'Bee' in metric),
             )
 
+    # Set x-axis title
     b = datetime.datetime.now()
     a = b - datetime.timedelta(days=1)
     fig.update_xaxes(title_text="Timestamp", insiderange=[a,b])
 
-    fig.update_yaxes(title_text="Bees | mbar | °C | °F | lbs | %", secondary_y=False, range=[0,100], fixedrange=True)
-    fig.update_yaxes(title_text="dB-klux", secondary_y=True, range=[0,50], fixedrange=True)
-    
     fig.update_layout(
         margin={'t':0,'l':0,'b':0,'r':0}
     )
     
+    # Set y-axes titles
+    fig.update_yaxes(title_text="°F | lbs | % | Δmbar", secondary_y=False, range=[0,140], fixedrange=True)
+    fig.update_yaxes(title_text="Bees | °C | dB-klux", secondary_y=True, range=[0,70], fixedrange=True)
+    
+    
     return fig
-
 
 parser = argparse.ArgumentParser(
             prog='beemetrics',
