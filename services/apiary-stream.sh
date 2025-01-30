c7a9b0bffb76368ec185018f4f45dcf578080fec
Benjamin Land <benland100@gmail.com>
2025-01-29 22:44:46 -0500

    Starting spring 2025 & some updates to scripts

--- services/apiary-stream.sh
+++ services/apiary-stream.sh
@@ -2,4 +2,4 @@
 
 cd /var/lib/beecam/webroot/stream
 
-exec ffmpeg -i rtsp://admin:honeybee@apiary.home.arpa:554/h265Preview_01_main -fflags flush_packets -max_delay 10 -flags -global_header -hls_time 5 -hls_list_size 10 -vf crop=1920:1080 -an -c:v h264_nvenc -preset medium -crf 18 -y apiary.m3u8
+exec ffmpeg -i rtsp://admin:honeybee@apiary.home.arpa:554/h265Preview_01_main -fflags flush_packets -max_delay 10 -flags -global_header -hls_time 5 -hls_list_size 10 -vf 'crop=2560:1440:640:250,scale=1920:1080' -an -c:v h264_nvenc -preset medium -crf 18 -y apiary.m3u8
