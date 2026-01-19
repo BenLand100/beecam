390a97f1ebbbfe7e19b90e32bc83d05f9af0cf10
Benjamin Land <benland100@gmail.com>
2026-01-19 11:37:19 -0500

    Experimenting with intel transcoding & fix phrasing

--- services/apiary-stream.sh
+++ services/apiary-stream.sh
@@ -2,4 +2,9 @@
 
 cd /var/lib/beecam/webroot/stream
 
-exec ffmpeg -i rtsp://admin:honeybee@apiary.home.arpa:554/h265Preview_01_main -fflags flush_packets -max_delay 10 -flags -global_header -hls_time 5 -hls_list_size 10 -vf 'crop=2560:1440:640:250,scale=1920:1080' -an -c:v h264_nvenc -preset medium -crf 18 -y apiary.m3u8
+# Nvidia nvenc
+#exec ffmpeg -i rtsp://admin:honeybee@apiary.home.arpa:554/h265Preview_01_main -fflags flush_packets -max_delay 10 -flags -global_header -hls_time 5 -hls_list_size 10 -vf 'crop=2560:1440:640:250,scale=1920:1080' -an -c:v h264_nvenc -preset slow -crf 18 -y apiary.m3u8
+
+# Intel QuickSync
+exec ffmpeg -vaapi_device /dev/dri/renderD128 -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i rtsp://admin:honeybee@apiary.home.arpa:554/h265Preview_01_main -fflags flush_packets -max_delay 10 -flags -global_header -hls_time 5 -hls_list_size 10 -vf "hwdownload,format=nv12,crop=2560:1440:640:250,scale=1920:1080,hwupload=extra_hw_frames=8,format=vaapi" -tune zerolatency -an -c:v h264_vaapi -preset balanced -qp 20 -y apiary.m3u8
+
--- services/hive2-stream.sh
+++ services/hive2-stream.sh
@@ -2,4 +2,8 @@
 
 cd /var/lib/beecam/webroot/stream
 
-exec ffmpeg -i rtsp://admin:honeybee@hive2.home.arpa:554/h265Preview_01_main -fflags flush_packets -max_delay 10 -flags -global_header -hls_time 5 -hls_list_size 10 -vf crop=1920:1080 -an -c:v h264_nvenc -preset medium -crf 18 -y hive2.m3u8
+# Nvidia nvenc
+exec ffmpeg -i rtsp://admin:honeybee@hive2.home.arpa:554/h265Preview_01_main -fflags flush_packets -max_delay 10 -flags -global_header -hls_time 5 -hls_list_size 10 -vf crop=1920:1080 -an -c:v h264_nvenc -preset p6 -crf 18 -y hive2.m3u8
+
+# Intel QuickSync
+#exec ffmpeg -vaapi_device /dev/dri/renderD128 -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i rtsp://admin:honeybee@hive2.home.arpa:554/h265Preview_01_main -fflags flush_packets -max_delay 10 -flags -global_header -hls_time 5 -hls_list_size 10 -vf "hwdownload,format=nv12,crop=1920:1080:960:540,hwupload=extra_hw_frames=8,format=vaapi" -tune zerolatency -an -c:v h264_vaapi -preset medium -qp 18 -y hive2.m3u8
